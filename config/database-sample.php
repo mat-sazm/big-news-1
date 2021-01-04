12bea5a0de82f0b3e54bfc2083155a6c7d758891
Arnaud BODEL <contact@osw3.net>
2021-01-04 11:51:00 +0100

    Config BDD

---
+++ config/database-sample.php
@@ -0,0 +1,10 @@
+<?php
+
+// configurations de connections aux bases de données par défaut
+$db_type = "mysql";
+$db_host = null;
+$db_port = 3306;
+$db_user = null;
+$db_pass = null;
+$db_schema = null;
+$db_charset = "utf8";
