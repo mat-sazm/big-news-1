2595c8983c296124e582f7a3bac9b05f8b0d96f7
Arnaud BODEL <contact@osw3.net>
2021-01-06 16:22:35 +0100

    add utils autoload

--- app/utils.php
+++ app/utils.php
@@ -13,20 +13,26 @@ if (!defined("UTILS_PATH"))
 // Si UTILS_PATH n'est pas définit à NULL, C.A.D que UTILS_PATH est bien définit en amont (dans le fichier config.php)
 if (UTILS_PATH != null)
 {
-    
     // Verifier l'existance du répertoire "utils"
+    if (is_dir( UTILS_PATH ))
+    {
 
         // Scanner le contenu du répertoire
-
+        $utils = scandir(UTILS_PATH);
 
         // Boucle sur le tableau du contenu du répertoire "utils"
-
+        foreach ($utils as $util)
+        {
             // Filtre les fichiers PHP
-
+            // if (preg_match( "/\.php$/i", $util))
+            // if (preg_match( "/\.(php|PHP)$/", $util))
+            // if (preg_match("/^.*\.php$/", $util))
+            if (preg_match("/\.php$/i", $util))
+            {
                 // Inclure le fichier PHP
+                include_once UTILS_PATH . $util;
+            }
 
-                // include_once "../utils/url.php";
-                // include_once "../utils/redirect.php";
-                // include_once "../utils/redirectToRoute.php";
-
+        }
+    }
 }
--- src/views/homepage/index.php
+++ src/views/homepage/index.php
@@ -3,10 +3,10 @@
 HOMEPAGE DATA
 <hr>
 
-<?= url("article_index") ?><br>
-<a href="<?= url("article_index") ?>">Lire nos articles</a>
-<br>
-<?= url("homepage") ?><br>
-<?= url("homepage", true) ?><br>
+
+<?php 
+$fruits = ["Pommes", "Bananes"];
+dump($fruits);
+?>
 
 <?php include_once "../src/views/layout/footer.php"; ?>
